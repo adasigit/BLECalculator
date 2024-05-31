d80ff6ca3b8c19418f4a4903d0e036060bb1f82c
Sigit Kurniawan <skurniawan24@bsd.idserve.net>
2024-05-31 14:33:52 +0700

    Initial Commit

---
+++ BLECalculator/BLECalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLECalculatorApp.swift
+//  BLECalculator
+//
+//  Created by Sigit Academy on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BLECalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
